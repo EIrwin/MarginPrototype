db84a003e33ebb5ebb07f9a01d0829385d135095
Eric Irwin <Eric.Irwin1124@gmail.com>
2014-12-27 14:56:39 -0700

    Added Edit Margins Window and Toggle Margins Button Functionality

---
+++ MarginPrototype/EditMarginsWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+using Telerik.Windows.Controls;
+
+namespace MarginPrototype
+{
+    /// <summary>
+    /// Interaction logic for EditMarginsWindow.xaml
+    /// </summary>
+    public partial class EditMarginsWindow : Window
+    {
+        private RadDiagramShape _marginShape;
+        private RadDiagram _diagram;
+        private Thickness _margin;
+
+        public EditMarginsWindow(RadDiagramShape marginShape,RadDiagram diagram,Thickness margin)
+        {
+            InitializeComponent();
+
+            _marginShape = marginShape;
+            _diagram = diagram;
+            _margin = margin;
+
+            this.DataContext = _margin;
+        }
+
+        private void SaveMarginsButton_Click(object sender, RoutedEventArgs e)
+        {
+            double top = double.Parse(TopMarginTextBox.Text);
+            double bottom = double.Parse(BottomMarginTextBox.Text);
+            double left = double.Parse(LeftMarginTextBox.Text);
+            double right = double.Parse(RightMarginTextBox.Text);
+
+            UpdateMargins(top,bottom,left,right);
+
+            Close();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void UpdateMargins(double top, double bottom, double left, double right)
+        {
+            _marginShape.Position = new Point(left, top);
+            _marginShape.Height = _diagram.ActualHeight - (top + bottom);
+            _marginShape.Width = _diagram.ActualWidth - (left + right);
+            _margin = new Thickness(left,top,right,bottom); //Not updating binding source
+        }
+
+
+    }
+}
--- MarginPrototype/MainWindow.xaml.cs
+++ MarginPrototype/MainWindow.xaml.cs
@@ -20,33 +20,48 @@ namespace MarginPrototype
     /// </summary>
     public partial class MainWindow : Window
     {
-        private RadDiagramShape _margin;
+        private RadDiagramShape _marginShape;
+
+        private const double DEFAULT_MARGIN = 20;
+
+        private Thickness _margin;
 
         public MainWindow()
         {
             InitializeComponent();
             InitializeMarginLines();
-
-            Diagram.SizeChanged += (o, e) =>
-                {
-                    _margin.Height = Diagram.ActualHeight - 20;
-                    _margin.Width = Diagram.ActualWidth - 20;
-                };
         }
 
         private void InitializeMarginLines()
         {
-            _margin = new RadDiagramShape();
-            _margin.Background = new SolidColorBrush(Colors.Transparent);
-            _margin.Position = new Point(20, 20);
-            _margin.Loaded += (o, e) =>
-                {
-                    var s = (RadDiagramShape)o;
-                    s.Height = Diagram.ActualHeight - 20;
-                    s.Width = Diagram.ActualWidth - 20;
-                };
+            _margin = new Thickness(DEFAULT_MARGIN);
 
-            Diagram.AddShape(_margin);
+            _marginShape = new RadDiagramShape();
+            _marginShape.Background = new SolidColorBrush(Colors.Transparent);
+            _marginShape.Position = new Point(_margin.Top,_margin.Left);
+            _marginShape.IsEnabled = false;
+            _marginShape.Loaded += UpdateMarginsShape;
+
+            Diagram.SizeChanged += UpdateMarginsShape;
+            Diagram.AddShape(_marginShape);
+        }
+
+        private void UpdateMarginsShape(object sender,EventArgs e)
+        {
+            _marginShape.Position = new Point(_margin.Top, _margin.Left);
+            _marginShape.Height = Diagram.ActualHeight - (_margin.Top + _margin.Bottom);
+            _marginShape.Width = Diagram.ActualWidth - (_margin.Left + _margin.Right);
+        }
+
+        private void ToggleMarginButton_Click(object sender, RoutedEventArgs e)
+        {
+            _marginShape.Visibility = _marginShape.Visibility == Visibility.Visible ? Visibility.Hidden : Visibility.Visible;
+        }
+
+        private void EditMarginButton_Click(object sender, RoutedEventArgs e)
+        {
+            EditMarginsWindow editMarginsWindow = new EditMarginsWindow(_marginShape,Diagram,_margin);
+            editMarginsWindow.Show();
         }
     }
 }
