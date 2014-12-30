e6fff78de185764f348de819f05d035666881b0b
Eric Irwin <Eric.Irwin1124@gmail.com>
2014-12-29 19:32:51 -0700

    Added 'New Page' functionality, as well as handling multiple pages and keeping track of page dimensions.

--- MarginPrototype/MainWindow.xaml.cs
+++ MarginPrototype/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Media;
 using MarginPrototype.Shapes;
@@ -17,52 +18,66 @@ namespace MarginPrototype
         private List<MarginShape> _marginShapes;
         private ReportViewModel _reportViewModel;
 
-        private const double DEFAULT_MARGIN = 20;
+        private const double DefaultMargin = 40;
+        private const double DefaultPageHeight = 750;
+
+        public int CurrentPage { get; set; }
 
         public MainWindow()
         {
             InitializeComponent();
 
-            _margin = new Thickness(DEFAULT_MARGIN);
+            _margin = new Thickness(DefaultMargin);
             _marginShapes = new List<MarginShape>();
 
+            InitializeDiagram();
             InitializeReport();
             InitializeFirstPage();
-            InitializeFirstPageMarginShape();
+
+            CurrentPage = 0;
+        }
+
+        private void InitializeDiagram()
+        {
+            Diagram.Height = DefaultPageHeight;
         }
 
         private void InitializeReport()
         {
             _reportViewModel = new ReportViewModel();
-
+            DataContext = _reportViewModel;
         }
 
         private void InitializeFirstPage()
         {
-            _reportViewModel.Pages.Add(new PageViewModel()
+            var pageViewModel = new PageViewModel()
             {
                 PageNumber = 1,
                 Top = 0,
-                Bottom = Diagram.ActualHeight
-            });
-        }
+                Bottom = DefaultPageHeight
+            };
 
-        private void InitializeFirstPageMarginShape()
-        {
-            var marginShape = new MarginShape();
-            marginShape.Position = new Point(_margin.Left,_margin.Top);
+            var marginShape = new MarginShape
+                {
+                    Position = new Point(_margin.Left, _margin.Top),
+                    DataContext = pageViewModel
+                };
             marginShape.Loaded += UpdateMarginsShape;
 
             _marginShapes.Add(marginShape);
+            _reportViewModel.Pages.Add(pageViewModel);
 
             Diagram.AddShape(marginShape);
+
         }
 
         private void UpdateMarginsShape(object sender,EventArgs e)
         {
             _marginShapes.ForEach(marginShape =>
                 {
-                    marginShape.Position = new Point(_margin.Left, _margin.Top);
+                    var pageViewModel = (PageViewModel) marginShape.DataContext;
+
+                    marginShape.Position = new Point(_margin.Left,pageViewModel.Top + _margin.Top);
                     marginShape.Height = Diagram.ActualHeight - (_margin.Top + _margin.Bottom);
                     marginShape.Width = Diagram.ActualWidth - (_margin.Left + _margin.Right);
                 });
@@ -76,7 +91,6 @@ namespace MarginPrototype
                                                   ? Visibility.Hidden
                                                   : Visibility.Visible;
                 });
-
         }
 
         private void EditMarginButton_Click(object sender, RoutedEventArgs e)
@@ -92,6 +106,43 @@ namespace MarginPrototype
 
         private void NewPageButton_Click(object sender, RoutedEventArgs e)
         {
+            var lastPage = _reportViewModel.Pages.Last();
+
+            var pageViewModel = new PageViewModel()
+                {
+                    Top = lastPage.Bottom + 1,
+                    Bottom = (lastPage.Bottom + 1) + DefaultPageHeight,
+                    PageNumber = lastPage.PageNumber + 1
+                };
+
+            var marginShape = new MarginShape
+                {
+                    Position = new Point(_margin.Left, pageViewModel.Top + _margin.Top),
+                    DataContext = pageViewModel
+                };
+
+            marginShape.Loaded += UpdateMarginsShape;
+            
+            _reportViewModel.Pages.Add(pageViewModel);
+            _marginShapes.Add(marginShape);
+
+            Diagram.AddShape(marginShape);
+        }
+
+        private void Diagram_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            //TODO: This should be easier with our actual integrated diagam
+            //TODO: Convert this to a math function that determines page based on height, margin and offset(s)
+
+            Point currentLocation = e.GetPosition((IInputElement) sender);
+            _reportViewModel.Pages.ForEach(p =>
+                {
+                    if (currentLocation.Y >= p.Top && currentLocation.Y <= p.Bottom)
+                    {
+                        CurrentPosition.Text = currentLocation.X + "," +
+                                               currentLocation.Y;
+                    }
+                });
 
         }
     }
